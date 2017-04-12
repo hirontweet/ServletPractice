ce0d87b24c805acc83afcf16b1787698dfb8d154
Hirofumi NAKANO <jasdfhnakano@gmail.com>
2017-04-12 18:09:16 +0900

    リクエスト情報の取得には無い項目を試しにやってみた
    doPost(), doGet()の他に、どのメソッドかを区別しない関数service(HttpServletRequest, HttpServletResponse)があったため、いろんなメソッドで実行結果が変わるか試してみた。
    JavaDocによると
    「public で定義された service メソッド経由で標準 HTTP リクエストを受け取り、それらをこのクラスで定義された doXXX メソッドにディスパッチします。」
    とある。そのため、自動的にメソッドを判別してdoXXXメソッドへ処理を流しているようである。後ほどソースコードを覗いてみたい。

---
+++ WEB-INF/src/ReceiveAllRequests.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ReceiveAllRequests extends HttpServlet {
+  public void service(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException{
+    
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>テスト</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+
+        sb.append("<p>");
+        sb.append("getScheme:");
+        sb.append(request.getScheme());
+        sb.append("</p>");
+
+        sb.append("<p>");
+        sb.append("getProtocol:");
+        sb.append(request.getProtocol());
+        sb.append("</p>");
+
+        sb.append("<p>");
+        sb.append("getMethod:");
+        sb.append(request.getMethod());
+        sb.append("</p>");
+
+        sb.append("</body>");
+        sb.append("</html>");
+
+        out.println(new String(sb));
+
+        out.close();
+        
+    }
+}
